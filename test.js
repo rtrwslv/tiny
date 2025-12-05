07805b302a178c70b825a1f7a1ae03b5b3b572c9
Ярослав <83144402+rtrwslv@users.noreply.github.com>
2025-12-05 16:18:50 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,47 +1,30 @@
-const { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
-
 function getAttachmentsByIndex(gDbView, index, callback) {
   let msgHdr = gDbView.getMsgHdrAt(index);
   if (!msgHdr) { callback([]); return; }
 
-  let folder = msgHdr.folder;
-  let msgUri = folder.getUriForMsg(msgHdr);
-  let msgService = MailServices.messageServiceFromURI(msgUri);
-
-  // Объект sink для разбора MIME
-  let sink = Cc["@mozilla.org/messenger/messageheader-sink;1"]
-               .createInstance(Ci.nsIMsgHeaderSink);
-  
-  sink.msgHdr = msgHdr;
-  sink.onStartMessage = function() {};
-  sink.onEndMessage = function() {
-    let attachments = [];
-    if (this.attachments && this.attachments.length) {
-      for (let att of this.attachments) {
-        attachments.push({
-          name: att.name,
-          size: att.size,
-          url: att.url,
-          contentType: att.contentType
-        });
-      }
-    }
-    callback(attachments);
-  };
-  
   let msgWindow = Cc["@mozilla.org/messenger/msgwindow;1"].createInstance(Ci.nsIMsgWindow);
-  
-  msgService.streamMessage(
-    msgUri,
-    sink,
-    null,
-    msgWindow,
-    false,
-    null
-  );
+
+  let cb = {
+    receiveMessage: function(mimeMsg) {
+      let attachments = [];
+      if (mimeMsg && mimeMsg.allAttachments && mimeMsg.allAttachments.length) {
+        for (let att of mimeMsg.allAttachments) {
+          attachments.push({
+            name: att.name,
+            size: att.size,
+            url: att.url,
+            contentType: att.contentType
+          });
+        }
+      }
+      callback(attachments);
+    }
+  };
+
+  MsgHdrToMimeMessage(msgHdr, msgWindow, false, cb);
 }
 
-// --- Использование ---
+// Использование
 getAttachmentsByIndex(gDbView, 0, attachments => {
   console.log("Вложения:", attachments);
   attachments.forEach(a => console.log(a.name, a.size, a.url, a.contentType));
