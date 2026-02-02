75bcbb520103333c532febbba4dff7c2c6284815
Ярослав <83144402+rtrwslv@users.noreply.github.com>
2026-02-02 13:29:04 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,29 +1,6 @@
-const indicator = document.getElementById("connectionIndicator");
-
-function showOfflineTooltipOnce() {
-  if (!indicator.classList.contains("status-offline")) {
-    return;
-  }
-
-  const tooltip = document.createElement("div");
-  tooltip.className = "connection-tooltip";
-  tooltip.textContent = "Пропало интернет-соединение";
-
-  indicator.appendChild(tooltip);
-
-  indicator.classList.remove("status-offline");
-  indicator.classList.add("status-offline-seen");
-
-  setTimeout(() => {
-    tooltip.remove();
-  }, 2500);
-}
-
-indicator.addEventListener("mouseenter", showOfflineTooltipOnce);
-
 #connectionIndicator .connection-tooltip {
   position: absolute;
-  top: 24px;
+  bottom: calc(100% + 15px); /* ← ВАЖНО */
   left: 50%;
   transform: translateX(-50%);
   background: var(--arrowpanel-background);
