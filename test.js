c96e02898146a14cf0f611c72b900c4154d9e005
Ярослав <83144402+rtrwslv@users.noreply.github.com>
2026-03-03 16:17:26 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,51 @@
 function getTemplatesFolderForTemplate() {
-  let identity = null;
-  
   const defaultAccount = MailServices.accounts.defaultAccount;
-  if (defaultAccount) {
-    identity = defaultAccount.defaultIdentity;
+  if (!defaultAccount) {
+    return null;
   }
 
-  if (identity?.stationeryFolder) {
+  const identity = defaultAccount.defaultIdentity;
+  if (!identity) {
+    return null;
+  }
+
+  if (identity.tmplFolderUri) {
     try {
-      const folder = MailUtils.getOrCreateFolder(identity.stationeryFolder);
-      if (folder) {
-        return folder;
+      const templatesFolder = MailUtils.getOrCreateFolder(identity.tmplFolderUri);
+      if (templatesFolder) {
+        return templatesFolder;
       }
     } catch {}
   }
 
-  if (defaultAccount) {
+  const pickerMode = identity.tmplFolderPickerMode;
+
+  if (pickerMode === 1) {
     try {
-      const rootFolder = defaultAccount.incomingServer.rootFolder;
-      const templatesFolder = rootFolder.getFolderWithFlags(Ci.nsMsgFolderFlags.Templates);
+      const server = defaultAccount.incomingServer;
+      const rootFolder = server.rootFolder;
+      let templatesFolder = rootFolder.getFolderWithFlags(Ci.nsMsgFolderFlags.Templates);
+      
+      if (!templatesFolder && rootFolder.canCreateSubfolders) {
+        templatesFolder = rootFolder.createLocalSubfolder("Templates");
+        templatesFolder.setFlag(Ci.nsMsgFolderFlags.Templates);
+      }
+      
+      if (templatesFolder) {
+        return templatesFolder;
+      }
+    } catch {}
+  } else if (pickerMode === 0) {
+    try {
+      const localServer = MailServices.accounts.localFoldersServer;
+      const localRoot = localServer.rootFolder;
+      let templatesFolder = localRoot.getFolderWithFlags(Ci.nsMsgFolderFlags.Templates);
+      
+      if (!templatesFolder && localRoot.canCreateSubfolders) {
+        templatesFolder = localRoot.createLocalSubfolder("Templates");
+        templatesFolder.setFlag(Ci.nsMsgFolderFlags.Templates);
+      }
+      
       if (templatesFolder) {
         return templatesFolder;
       }
